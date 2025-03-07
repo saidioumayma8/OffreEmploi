3bb958ccfbdd678a3050e5f138c7f0315f95331b
youssfmadani <youssfelmadani@gmail.com>
2025-03-07 10:42:49 +0000

    modification sur candidateurServlet

--- src/main/Java/Utils/DatabaseConnection.java
+++ src/main/Java/Utils/DatabaseConnection.java
@@ -6,20 +6,16 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    // URL de la base de données MySQL
-    private static final String URL = "jdbc:mysql://127.0.0.1:3305/OffreEmploi";
+    private static final String URL = "jdbc:mysql://localhost:3306/OffreEmploi";
     private static final String USER = "root";
     private static final String PASSWORD = "admin";
 
-    // Méthode pour obtenir une connexion à la base de données
     public static Connection getConnection() throws SQLException {
         try {
-            // Enregistrer le driver JDBC MySQL
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            // Connexion à la base de données
             Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("✅ Database connection successful!"); // Message de confirmation
+            System.out.println("✅ Database connection successful!");
 
             return connection;
         } catch (ClassNotFoundException e) {
